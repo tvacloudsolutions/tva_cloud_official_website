31da903f77019abad0d3804b901033df8121c3d6
tvacloudsolutions <86782363+tvacloudsolutions@users.noreply.github.com>
2021-07-01 17:45:19 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+include_once('index.html');
+
+/*
+
+if(isset($_POST['submit'])){
+
+$firstname = $_POST['firstname'];
+$lastname = $_POST['lastname'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$idtype = $_POST['IDtype'];
+$idnum = $_POST['idnum'];
+$radioVal = $_POST['rad'];
+$storage = $_POST['storage'];
+$amount = $_POST['amount'];
+$text = $_POST['text'];
+
+
+}
+
+$message = "FirstName:" + $firstname + "<br>" + "LastName:" + $lastname + "<br>" + "Email :" + $email + 
+        "<br>" + "Phone:" + $phone + "<br>" + "ID type :" + $idtype + "<br>" + "ID number :" + $idnum + 
+        "<br>" + "For :" + $radioVal + "<br>" + "Storage :" + $storage + "<br>" + "Amount :" + $amount +
+         "<br>" + "Additional Message :" + $text + "<br>" ;
+
+$to_email = '1by17cs044@bmsit.in';
+$subject = 'Customer Details';
+
+
+mail($to_email,$subject,$message);
+
+echo "<h1>Thank you for connecting with us. We will contact you shortly</h1>"
+*/
+?>
